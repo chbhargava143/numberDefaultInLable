89e49686dbd67628ffe9f503cf431f1f3a88c38f
bhargava <saibhargava54@gmail.com>
2021-02-13 20:09:01 +0530

    default

--- tik/ViewController.swift
+++ tik/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 var number = 0
+    var aView : UIView?
+    let activateIndicator = UIActivityIndicatorView()
+               
     @IBOutlet weak var numberDisplayLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         number = UserDefaults.standard.integer(forKey: "points1")
         numberDisplayLbl.text = "\(number)"
-//
+        activateIndicator.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        view.addSubview(activateIndicator)
 //        UserDefaults.standard.synchronize()
 //         UserDefaults.standard.value(forKey: "myNumber")
     }
 
+    @IBOutlet weak var userImage: UIImageView!
     @IBAction func incrNumber(_ sender: Any) {
        
        number += 1
@@ -35,6 +40,37 @@ var number = 0
         }
         UserDefaults.standard.set(numberDisplayLbl.text, forKey: "points1")
         
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if touch?.view == userImage{
+            print("Bhargava")
+            
+              performSegue(withIdentifier: "stVcs", sender: self)
+        }else {
+           
+            print("Nothing")
+            
+        }
+    }
+    func showSpinner(){
+        aView = UIView(frame: self.view.bounds)
+        aView?.backgroundColor = UIColor(white: 0, alpha: 0.5)
+
+        let ai = UIActivityIndicatorView(style: .large)
+        ai.center = aView!.center
+        ai.color = .red
+        ai.startAnimating()
+        aView?.addSubview(ai)
+        self.view.addSubview(aView!)
+
+        Timer.scheduledTimer(withTimeInterval: 20, repeats: false, block: {_ in self.removeSpinner()})
+    }
+
+    func removeSpinner(){
+
+        aView?.removeFromSuperview()
+        aView = nil
     }
 }
 
---
+++ tik/stVc.swift
@@ -0,0 +1,30 @@
+//
+//  stVc.swift
+//  tik
+//
+//  Created by bhargava on 13/02/21.
+//  Copyright © 2021 bhargava. All rights reserved.
+//
+
+import UIKit
+
+class stVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
