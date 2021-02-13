8fbaef623485f22a7f0974457f5abded52e4872d
bhargava <saibhargava54@gmail.com>
2021-02-13 09:48:02 +0530

    Initial Commit

---
+++ tik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tik
+//
+//  Created by bhargava on 13/02/21.
+//  Copyright © 2021 bhargava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
