f998b9dc893d110c656aa70cff4c236cb35a8b2f
bhargava <saibhargava54@gmail.com>
2021-02-13 15:50:37 +0530

    number default

--- tik/ViewController.swift
+++ tik/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var number = 0
+    @IBOutlet weak var numberDisplayLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        number = UserDefaults.standard.integer(forKey: "points1")
+        numberDisplayLbl.text = "\(number)"
+//
+//        UserDefaults.standard.synchronize()
+//         UserDefaults.standard.value(forKey: "myNumber")
     }
 
-
+    @IBAction func incrNumber(_ sender: Any) {
+       
+       number += 1
+        numberDisplayLbl.text = "\(number)"
+        UserDefaults.standard.set(numberDisplayLbl.text, forKey: "points1")
+    }
+    
+    @IBAction func dicNumber(_ sender: Any) {
+        if number > 0{
+            number -= 1
+            numberDisplayLbl.text = "\(number)"
+            
+        }
+        UserDefaults.standard.set(numberDisplayLbl.text, forKey: "points1")
+        
+    }
 }
 
